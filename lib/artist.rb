e64d4de920beb80de440e3cfa36d40f4ef43b987
nikifi <nikifiona@gmail.com>
2018-02-11 10:16:44 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+
+	attr_accessor :songs, :name
+      @@song_count = 0
+
+	def initialize(name)
+		@name = name
+		@songs = []
+
+	end
+
+	def add_song(song)
+      self.songs << song
+      song.artist = self
+       @@song_count += 1
+	end
+
+	def add_song_by_name(song_name)
+
+		song = Song.new(song_name)
+		self.songs << song
+		song.artist = self
+		 @@song_count += 1
+
+	end
+
+	def self.song_count
+     
+      @@song_count
+	end
+
+end
